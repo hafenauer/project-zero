1f3763e494b50d57978d802fb76a6e354f16b7a6
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-10 21:26:22 +0000

    Auto testing update

--- test_e-ink02.py
+++ test_e-ink02.py
@@ -93,22 +93,22 @@ def update_screen():
     img_b = Image.new('1', (epd.width, epd.height), 255)
     img_r = Image.new('1', (epd.width, epd.height), 255)
     
-    avatar_path = os.path.join(script_dir, "assets", "images", "avatar.gif")
+    avatar_path = os.path.join(script_dir, "assets", "images", "avatar16.gif")
     
     try:
         avatar = Image.open(avatar_path)
-        img_b.paste(avatar, (4, 4))
+        img_b.paste(avatar, (2, 2))
     except Exception as e:
         print(f"Error loading avatar: {e}")
 
     draw_b = ImageDraw.Draw(img_b)
-    draw_b.rectangle((0, 0, 40, 40), outline=0)  
+    draw_b.rectangle((0, 0, 20, 20), outline=0)
 
-    draw_b.text((42, 0), hostname, font=font_mono_small, fill=0)
-    draw_b.text((42, 11), ip_addr, font=font_mono_tiny, fill=0)
-    draw_b.text((42, 21), uptime, font=font_mono_tiny, fill=0)
-    draw_b.text((42, 31), f"{cpu_temp}C | {signal}% ", font=font_mono_tiny, fill=0)
-    draw_b.text((42, 41), load_avg, font=font_mono_tiny, fill=0)
+    draw_b.text((22, 0), hostname, font=font_mono_small, fill=0)
+    draw_b.text((22, 11), ip_addr, font=font_mono_tiny, fill=0)
+    draw_b.text((0, 21), uptime, font=font_mono_tiny, fill=0)
+    draw_b.text((0, 31), f"{cpu_temp}C | {signal}% ", font=font_mono_tiny, fill=0)
+    draw_b.text((0, 41), load_avg, font=font_mono_tiny, fill=0)
 
     # Vertical orientation
     img_b, img_r = img_b.rotate(180), img_r.rotate(180)
