8a3c9f89563ace052a9c02682bcd01a2c228159a
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-11 17:04:17 +0000

    Auto testing update

--- test_e-ink02.py
+++ test_e-ink02.py
@@ -218,9 +218,9 @@ def update_screen():
 
     draw_r.text((-1, start_y), "Temperature", font=font_mono_label, fill=0)
 
-    draw_b.text((-1, start_y + 0 * row_gap + 10), "00.0", font=font_mono_large, fill=0)
-    draw_b.text((right_edge, start_y + 0 * row_gap + 12), "°C", font=font_mono_medium, fill=0, anchor="ra")
-    draw_b.text((right_edge, start_y + 0 * row_gap + 26), "00.0", font=font_mono_medium, fill=0, anchor="ra")
+    draw_b.text((-1, start_y + 0 * row_gap + 8), "00.0", font=font_mono_large, fill=0)
+    draw_b.text((80, start_y + 0 * row_gap + 9), "°C", font=font_mono_medium, fill=0)
+    draw_b.text((80, start_y + 0 * row_gap + 22), "00.0", font=font_mono_medium, fill=0)
 
     ### Humidity - inside and outside
 
