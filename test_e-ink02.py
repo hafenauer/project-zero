5e6752008f2efcb3895a6935406d07253ad06621
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-11 19:44:24 +0000

    Auto testing update

--- test_e-ink02.py
+++ test_e-ink02.py
@@ -260,14 +260,14 @@ def update_screen():
     
     draw_r.text((-1, start_y + 2 * row_gap), "Air Quality", font=font_mono_label, fill=0)
 
-    draw_b.text((-1, start_y + 2 * row_gap + 15), "VOC", font=font_mono_tiny, fill=0)
+    draw_b.text((-1, start_y + 2 * row_gap + 15), "VOC", font=font_mono_small, fill=0)
     draw_b.text((24, start_y + 2 * row_gap + 12), "100", font=font_mono_readout_medium, fill=0)
-    draw_b.text((-1, start_y + 2 * row_gap + 27), "NOx", font=font_mono_tiny, fill=0)
+    draw_b.text((-1, start_y + 2 * row_gap + 27), "NOx", font=font_mono_small, fill=0)
     draw_b.text((24, start_y + 2 * row_gap + 24), "001", font=font_mono_readout_medium, fill=0)
 
-    draw_b.text((65, start_y + 2 * row_gap + 15), "PM", font=font_mono_small, fill=0)
+    draw_b.text((60, start_y + 2 * row_gap + 15), "PM", font=font_mono_small, fill=0)
     draw_b.text((85, start_y + 2 * row_gap + 12), "25", font=font_mono_medium, fill=0)
-    draw_b.text((65, start_y + 2 * row_gap + 27), "AQI", font=font_mono_small, fill=0)
+    draw_b.text((60, start_y + 2 * row_gap + 27), "AQI", font=font_mono_small, fill=0)
     draw_b.text((85, start_y + 2 * row_gap + 24), "5", font=font_mono_medium, fill=0)
 
     # Bottom info section
