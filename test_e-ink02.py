9bb44cc219c5432113ab44e4352191bae5d46ba6
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-11 20:13:47 +0000

    Auto testing update

--- test_e-ink02.py
+++ test_e-ink02.py
@@ -240,7 +240,7 @@ def update_screen():
     
     tri_x = right_edge - 8
     tri_y = start_y + 0 * row_gap + 17
-    draw_isosceles_triangle(draw_b, x=tri_x, y=tri_y, width=10, height=8, direction='down', fill=0)
+    draw_isosceles_triangle(draw_r, x=tri_x, y=tri_y, width=10, height=8, direction='down', fill=0)
     
     draw_b.text((71, start_y + 0 * row_gap + 24), "00.0", font=font_mono_medium, fill=0)
 
@@ -253,7 +253,7 @@ def update_screen():
     
     tri_x = right_edge - 8
     tri_y = start_y + 1 * row_gap + 17
-    draw_isosceles_triangle(draw_b, x=tri_x, y=tri_y, width=10, height=8, direction='up', fill=0)
+    draw_isosceles_triangle(draw_r, x=tri_x, y=tri_y, width=10, height=8, direction='up', fill=0)
     
     draw_b.text((71, start_y + 1 * row_gap + 24), "00.0", font=font_mono_medium, fill=0)
 
