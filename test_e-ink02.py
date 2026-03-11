d6529390e5707981532a32143ec83c6781648575
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-11 20:07:20 +0000

    Auto testing update

--- test_e-ink02.py
+++ test_e-ink02.py
@@ -266,10 +266,10 @@ def update_screen():
     draw_b.text((-1, start_y + 2 * row_gap + 27), "NOx", font=font_mono_small, fill=0)
     draw_b.text((24, start_y + 2 * row_gap + 24), "001", font=font_mono_readout_medium, fill=0)
 
-    draw_b.text((56, start_y + 2 * row_gap + 15), "PM", font=font_mono_small, fill=0)
-    draw_b.text((81, start_y + 2 * row_gap + 12), "25", font=font_mono_medium, fill=0)
-    draw_b.text((56, start_y + 2 * row_gap + 27), "AQI", font=font_mono_small, fill=0)
-    draw_b.text((81, start_y + 2 * row_gap + 24), "5", font=font_mono_medium, fill=0)
+    draw_b.text((55, start_y + 2 * row_gap + 15), "PM", font=font_mono_small, fill=0)
+    draw_b.text((80, start_y + 2 * row_gap + 12), "25", font=font_mono_medium, fill=0)
+    draw_b.text((55, start_y + 2 * row_gap + 27), "AQI", font=font_mono_small, fill=0)
+    draw_b.text((80, start_y + 2 * row_gap + 24), "5", font=font_mono_medium, fill=0)
 
     # Bottom info section
     row_gap = 9
