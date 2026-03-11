f5899766396095e5bc28ae452b275b3d004b4b3e
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-11 19:00:29 +0000

    Auto testing update

--- test_e-ink02.py
+++ test_e-ink02.py
@@ -263,8 +263,8 @@ def update_screen():
     draw_b.text((-1, start_y + 2 * row_gap + 12), "VOC 100", font=font_mono_readout_medium, fill=0)
     draw_b.text((-1, start_y + 2 * row_gap + 24), "NOx 001", font=font_mono_readout_medium, fill=0)
 
-    draw_b.text((60, start_y + 2 * row_gap + 12), "PM 25", font=font_mono_medium, fill=0)
-    draw_b.text((60, start_y + 2 * row_gap + 24), "AQI 5", font=font_mono_medium, fill=0)
+    draw_b.text((65, start_y + 2 * row_gap + 12), "PM 25", font=font_mono_medium, fill=0)
+    draw_b.text((65, start_y + 2 * row_gap + 24), "AQI 5", font=font_mono_medium, fill=0)
 
     draw_b.line((0, start_y + 2 * row_gap + 39, right_edge, start_y + 2 * row_gap + 39), fill=0, width=1)
 
