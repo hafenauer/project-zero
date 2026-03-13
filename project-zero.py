f8819a540f30eef5f6b211c50c5ac773a6001d0f
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-13 22:21:04 +0000

    Auto testing update

--- project-zero.py
+++ project-zero.py
@@ -67,7 +67,7 @@ try:
     font_mono_medium         = ImageFont.truetype(font_path_ibm_plex_mono, 12)
     font_mono_readout_medium = ImageFont.truetype(font_path_roboto_mono_semibold, 13)
     font_mono_readout_large  = ImageFont.truetype(font_path_roboto_mono, 28)
-    font_mono_label          = ImageFont.truetype(font_path_atkinson_hyperlegible_next, 14)
+    font_mono_label          = ImageFont.truetype(font_path_atkinson_hyperlegible_next, 12)
 except Exception:
     font_mono_tiny = font_mono_small = font_mono_medium = font_mono_readout_medium = font_mono_readout_large = font_mono_label = ImageFont.load_default()
 
