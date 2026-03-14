f18e1e132e1bee07bd43b839e4505fc6562c8375
hafenauer <78387761+hafenauer@users.noreply.github.com>
2026-03-14 15:14:41 +0000

    Auto testing update

--- project-zero.py
+++ project-zero.py
@@ -141,7 +141,6 @@ def send_discovery_packet():
     
     voc_config = {**base_config,
         "name": "VOC Index",
-        "device_class": "volatile_organic_compounds",
         "state_class": "measurement",
         "unit_of_measurement": "Index",
         "value_template": "{{ value_json.voc_index }}",
@@ -150,7 +149,6 @@ def send_discovery_packet():
     
     nox_config = {**base_config,
         "name": "NOx Index",
-        "device_class": "nitrogen_dioxide",
         "state_class": "measurement",
         "unit_of_measurement": "Index",
         "value_template": "{{ value_json.nox_index }}",
